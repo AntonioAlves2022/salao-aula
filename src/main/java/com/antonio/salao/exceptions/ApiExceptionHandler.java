e2807fded1ffff87732f0ba23ea9ab1f3724f407
Antonio <antonio.alves@duque.g12.br>
2026-05-27 20:51:47 -0300

    27.05.2026 - Controller

--- src/main/java/com/antonio/salao/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/antonio/salao/exceptions/ApiExceptionHandler.java
@@ -1,10 +1,34 @@
 package com.antonio.salao.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
-    public ResponseEntity<?> handleRegraNegocio(){
+
+    @ExceptionHandler(RegraNegocioException.class)
+    public ResponseEntity<?> handleRegraNegocio(RegraNegocioException ex){
+        Map<String, Object> erro = new HashMap<>();
+        erro.put("data_hora", LocalDateTime.now());
+        erro.put("status_code", 400);
+        erro.put("message", ex.getMessage());
+        return ResponseEntity.badRequest().body(erro);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?>
+    handleNotFound(RecursoNaoEncontradoException ex){
+        Map<String, Object> erro = new HashMap<>();
+        erro.put("data_hora", LocalDateTime.now());
+        erro.put("status_code", 404);
+        erro.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
 }
--- src/main/java/com/antonio/salao/services/ClienteService.java
+++ src/main/java/com/antonio/salao/services/ClienteService.java
@@ -3,6 +3,7 @@ package com.antonio.salao.services;
 import com.antonio.salao.domain.dto.ClienteRequestDTO;
 import com.antonio.salao.domain.dto.ClienteResponseDTO;
 import com.antonio.salao.domain.entities.Cliente;
+import com.antonio.salao.exceptions.RegraNegocioException;
 import com.antonio.salao.repositories.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,16 @@ import org.springframework.stereotype.Service;
 public class ClienteService {
     private final ClienteRepository repository;
 
-    public ClienteResponseDTO salvar(ClienteRequestDTO dto){
+    public ClienteResponseDTO salvar(ClienteRequestDTO dto) throws RegraNegocioException {
+        // Validação do E-mail
+        if(repository.existsByEmail(dto.email())){
+            throw new RegraNegocioException("E-mail já cadastrado");
+        }
+
+        if(repository.existsByTelefone(dto.telefone())){
+            throw new RegraNegocioException("Telefone já cadastrado");
+        }
+
         Cliente cliente = Cliente.builder()
                 .nome(dto.nome())
                 .telefone(dto.telefone())
