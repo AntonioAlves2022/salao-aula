61724f254e85f03fc855cde8ee7739d9ede7e90a
Antonio <antonio.alves@duque.g12.br>
2026-05-27 19:41:34 -0300

    27.05.2026 - ExceptionHandler

---
+++ src/main/java/com/antonio/salao/exceptions/ApiExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.antonio.salao.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    public ResponseEntity<?> handleRegraNegocio(){
+    }
+}
