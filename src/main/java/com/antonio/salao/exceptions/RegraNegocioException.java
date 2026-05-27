b328f8b4dff8c1454ae4e1778dd72a0f22c12b92
Antonio <antonio.alves@duque.g12.br>
2026-05-27 19:30:45 -0300

    27.05.2026 - ExceptionHandler

---
+++ src/main/java/com/antonio/salao/exceptions/RegraNegocioException.java
@@ -0,0 +1,8 @@
+package com.antonio.salao.exceptions;
+
+
+public class RegraNegocioException extends Exception{
+    public RegraNegocioException(String mensagem) {
+        super(mensagem);
+    }
+}
